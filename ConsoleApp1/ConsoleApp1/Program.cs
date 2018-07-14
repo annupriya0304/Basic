616edf276f197e8734186a68a42fcf3b40bd13c2
annpriya <annupriya0304@gmail.com>
2018-07-14 19:45:45 +0530

    Added new

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = DateTime.Now;
+            string abc = dt.Date.ToShortDateString(); 
+
+            Console.WriteLine(abc);
+            Console.ReadKey();
+        }
+    }
+}
